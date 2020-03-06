3db23faf7cd3c44e69df4aa834888bb9b1d944a3
coldice1915 <coldice.zs44@gmail.com>
2020-03-05 18:38:34 -0800

    create home route

--- app.py
+++ app.py
@@ -2,13 +2,31 @@ import os
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from models import db_drop_and_create_all, setup_db, Actor, Movie
+from auth import AuthError, requires_auth
 
 def create_app(test_config=None):
-  # create and configure the app
-  app = Flask(__name__)
-  CORS(app)
+    # create and configure the app
+    app = Flask(__name__)
+    setup_db(app)
+    CORS(app)
+    db_drop_and_create_all()
 
-  return app
+    '''
+    Routes
+    '''
+    # homepage
+    @app.route('/')
+    def homepage():
+        return 'Come see all our actors and movies'
+    
+    # get all actors
+    @app.route('/actors')
+    @requires_auth
+
+
+
+    return app
 
 APP = create_app()
 
