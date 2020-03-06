7484ddcd7b94aaa9d183cd79084920d8f9ab5460
coldice1915 <coldice.zs44@gmail.com>
2020-03-05 18:38:13 -0800

    change database name

--- models.py
+++ models.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
 
-database_name = "casting"
+database_name = "casting-agency"
 database_path = "postgres://{}/{}".format('localhost:5432', database_name)
 
 db = SQLAlchemy()
